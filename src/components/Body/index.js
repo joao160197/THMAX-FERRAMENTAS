10d75ec804f0092553ce32e8feb8b91d7922daa0
GCLourenco <gcoradello@outlook.com>
2024-08-27 22:59:29 -0300

    Body upgrade

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import backgroundImg from '../../img/Background.jpg';
 import serrasImg from '../../img/Serras.jpg';
@@ -9,19 +9,28 @@ import brocasEspeciaisImg from '../../img/Brocas Especiais.jpg';
 import variadasImg from '../../img/Variadas.jpg';
 import marcasImg from '../../img/Marcas.jpg';
 
+import deWaltLogo from '../../img/DeWalt-Logo.jpg';
+import rhomLogo from '../../img/Rohm-logo.jpg';
+import osgLogo from '../../img/OSG-logo.png';
+import gedoreLogo from '../../img/Gedore-logo.png';
+import starretLogo from '../../img/Starrett-logo.jpg';
+import indacoLogo from '../../img/Indaco-logo.png';
+
 export function Body() {
+  const [showImage, setShowImage] = useState(false);
+
   return (
     <main className="bg-gray-950">
       <div className="relative mb-6">
         <img src={backgroundImg} alt="Imagem de fundo" className="w-full h-128 object-cover" />
         <div className="absolute inset-0 flex items-center justify-center">
-          <h2 className="text-white text-5xl font-bold animate__animated animate__fadeInLeft">
+          <h2 className="text-white text-5xl font-bold animate__animated animate__fadeInLeft text-center">
             FERRAMENTAS ESPECIAIS
             <hr />
             Para Todos os Profissionais
           </h2>
           <hr />
-          <h3 className="text-white text-base font-bold animate__animated animate__fadeInLeft">THMAX Ferramentas</h3>
+          <h3 className="text-white text-base font-bold animate__animated animate__fadeInLeft text-center">THMAX Ferramentas</h3>
         </div>
       </div>
       <div className="relative my-6 mx-8">
@@ -40,9 +49,33 @@ export function Body() {
         {renderImageSection("Ferramentas Variadas", variadasImg)}
         {renderImageSection("Diversas Marcas", marcasImg)}
       </div>
+
+      <div className="relative flex flex-col items-center justify-center">
+        <div
+          onMouseEnter={() => setShowImage(true)}
+          onMouseLeave={() => setShowImage(false)}
+          className="text-white text-2xl cursor-pointer p-4"
+        >
+          <p className='bg-gray-800 rounded-lg p-1 text-center'>Marcas Parceiras</p>
+        </div>
+
+        <div
+          className={`flex flex-wrap justify-center mb-6 gap-6 transition-opacity duration-500 ${
+            showImage ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          <img src={deWaltLogo} alt="Logo DeWalt" className="w-32 h-24 object-cover rounded-lg" />
+          <img src={rhomLogo} alt="Logo Rhom" className="w-32 h-24 object-cover rounded-lg" />
+          <img src={osgLogo} alt="Logo OSG" className="w-32 h-24 object-cover rounded-lg" />
+          <img src={gedoreLogo} alt="Logo Gedore" className="w-32 h-24 object-cover rounded-lg" />
+          <img src={starretLogo} alt="Logo Starrett" className="w-32 h-24 object-cover rounded-lg" />
+          <img src={indacoLogo} alt="Logo Indaco" className="w-32 h-24 object-cover rounded-lg" />
+        </div>
+      </div>
     </main>
   );
 }
+
 function renderImageSection(title, imgSrc) {
     return (
       <div className="text-center">
@@ -62,4 +95,4 @@ function renderImageSection(title, imgSrc) {
         </span>
       </div>
     );
-  }
+}
