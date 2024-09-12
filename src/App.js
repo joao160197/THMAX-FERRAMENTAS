87fa7c8484f35107068d662636424afb4464ae6d
GCLourenco <gcoradello@outlook.com>
2024-09-12 19:45:04 -0300

    ContactPage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Header } from "./components/Header";
 import { Slyder } from "./components/Slyder";
 import { Body } from "./components/Body";
+import ContactPage from "./pages/ContactPage/ContactPage"; // Certifique-se de que o caminho está correto
 import { Footer } from "./components/Footer";
 
 import './App.css';
 
+// Componente Wrapper para condicionar a renderização de Slyder e Body
+const Wrapper = () => {
+  const location = useLocation();
+  const showSlyderAndBody = location.pathname === '/'; // Exibe Slyder e Body apenas na rota Home
+
+  return (
+    <>
+      {showSlyderAndBody && (
+        <>
+          <Slyder />
+          <Body />
+        </>
+      )}
+      {/* Renderize a rota ContactPage ou outras rotas aqui */}
+      <Routes>
+        <Route path="/contato" element={<ContactPage />} />
+        {/* Adicione outras rotas conforme necessário */}
+      </Routes>
+    </>
+  );
+}
+
 function App() {
   return (
-   <>
-   <Header/>
-   <Slyder/>
-   <Body/>
-   <Footer/>
-   </>
+    <Router>
+      <Header />
+      <Wrapper />
+      <Footer />
+    </Router>
   );
 }
 
